da8c3858233e6154ebc7f59efb051c66fbf997d0
Stephen Ball <sdball@gmail.com>
2012-03-05 13:37:42 -0500

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'date'
+Todo.create({name: 'Twitter Bootstrap Rails Todo App', description: 'Write a Rails application using Twitter Bootstrap to track todo items.', due_at: DateTime.now})
+Todo.create({name: 'Create db seeds', description: 'Write db seeds to populate todo items.', complete: true})
