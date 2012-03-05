4c7bede4ed547247d3404ad987f00fa40d3f23eb
Stephen Ball <sdball@gmail.com>
2012-03-05 16:56:14 -0500

    scope active/complete todos

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,7 @@
 class Todo < ActiveRecord::Base
+  scope :active, where(complete: false)
+  scope :complete, where(complete: true)
+
   def due_today?
     due_at && due_at.today?
   end
--- test/model/todo_test.rb
+++ test/model/todo_test.rb
@@ -15,4 +15,18 @@ describe Todo do
     todo = Todo.create!({task: "Test", due_at: 1.day.ago})
     todo.overdue?.must_equal true
   end
+
+  it "identifies complete todos" do
+    todo = Todo.create!({task: "Test1", complete: true})
+    Todo.complete.length.wont_equal 0
+    Todo.complete.must_include todo
+    Todo.complete.first.complete.must_equal true
+  end
+
+  it "identifies active todos" do
+    todo = Todo.create!({task: "Test1"})
+    Todo.active.length.wont_equal 0
+    Todo.active.must_include todo
+    Todo.active.first.complete.wont_equal true
+  end
 end
