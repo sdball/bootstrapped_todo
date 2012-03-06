20403319cbf6df06d06ef0155fc7b251c0462f9a
Stephen Ball <sdball@gmail.com>
2012-03-06 16:42:13 -0500

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
 require 'date'
-Todo.create({name: 'Twitter Bootstrap Rails Todo App', description: 'Write a Rails application using Twitter Bootstrap to track todo items.', due_at: DateTime.now})
-Todo.create({name: 'Create db seeds', description: 'Write db seeds to populate todo items.', complete: true})
+Todo.create({task: 'Twitter Bootstrap Rails Todo App', description: 'Write a Rails application using Twitter Bootstrap to track todo items.', due_at: Date.today})
+Todo.create({task: 'Create db seeds', description: 'Write db seeds to populate todo items.', complete: true})
