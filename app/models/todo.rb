2828104d091bf3db10128e02e2a6b0e967d71d2d
Stephen Ball <sdball@gmail.com>
2012-03-06 20:25:44 -0500

    order scopes by due_at

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,10 +1,11 @@
 class Todo < ActiveRecord::Base
-  scope :active, where(complete: false)
-  scope :complete, where(complete: true)
+  scope :active, where(complete: false).order('due_at DESC')
+  scope :complete, where(complete: true).order('due_at DESC')
 
   def due_today?
     due_at && due_at.today?
   end
+
   def overdue?
     due_at && due_at.past?
   end
